eaa6bc0ce72e1c3dc6d2fffc7fce9a51946abaef
st9265742-afk <st9265742@stud.duikt.edu.ua>
2025-09-29 11:30:04 +0000

    add Lab1

---
+++ Lab1/Task2/task2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Program
+    {
+        private static Random random = new Random();
+        public static int[] GenerateRandomArray(int size, int min, int max)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(min, max + 1);
+            }
+            return array;
+        }
+        public static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int value in numbers)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+        public static double GetAverage(int[] numbers)
+        {
+            return (double)GetSum(numbers) / numbers.Length;
+        }
+        public static int GetMin(int[] numbers)
+        {
+            int min = numbers[0];
+            foreach (int value in numbers)
+            {
+                if (value < min)
+                    min = value;
+            }
+            return min;
+        }
+        public static int GetMax(int[] numbers)
+        {
+            int max = numbers[0];
+            foreach (int value in numbers)
+            {
+                if (value > max)
+                    max = value;
+            }
+            return max;
+        }
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ Lab1/Task5/task5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task5;
+
+public class Program
+{
+    public static double GetAverage(int[] marks)
+    {
+        int sum = 0;
+        foreach (int mark in marks)
+            sum += mark;
+        return (double)sum / marks.Length;
+    }
+    public static int GetMin(int[] marks)
+    {
+        int min = marks[0];
+        foreach (int mark in marks)
+            if (mark < min)
+                min = mark;
+        return min;
+    }
+    public static int GetMax(int[] marks)
+    {
+        int max = marks[0];
+        foreach (int mark in marks)
+            if (mark > max)
+                max = mark;
+        return max;
+    }
+    void PrintGroupStatistics(int[][] groups)
+    {
+        for (int i = 0; i < groups.Length; i++)
+        {
+            int[] group = groups[i];
+            double avg = GetAverage(group);
+            int min = GetMin(group);
+            int max = GetMax(group);
+
+            Console.WriteLine($"Група {i + 1}:");
+            Console.WriteLine($"Середня оцінка: {avg:F2}");
+            Console.WriteLine($"Мінімальна оцінка: {min}");
+            Console.WriteLine($"Максимальна оцінка: {max}");
+            Console.WriteLine();
+        }
+    }
+    public static void Main()
+    {
+
+    }
+}
